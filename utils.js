c0f579ebbe7754434d6e21243ff3e290370864ee
egaliteDev <121259896+egaliteDev@users.noreply.github.com>
2022-12-24 12:10:48 +0200

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,16 @@
+/**
+ * ! Удаление дубликатов из массива
+ */
+
+{
+let chars = ['A', 'B', 'A', 'C', 'B'];
+let uniqueChars = [...new Set(chars)]
+}
+
+/**
+ * ! Длина Set
+ */
+{
+let chars = ['A', 'B', 'A', 'C', 'B'];
+let uniqueCharsLength = [...new Set(chars)].size
+}
